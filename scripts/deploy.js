c593fb616ac1a31f4de7e3bb0efdd37cfd01038f
svipas <benas.svipas@gmail.com>
2020-03-04 20:02:29 +0200

    Use IIFE for deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const Parcel = require('parcel-bundler');
 
-async function main() {
+(async function main() {
   // Remove dist dir
   const distPath = path.resolve('dist');
   await fs.promises.rmdir(distPath, { recursive: true });
@@ -26,6 +26,4 @@ async function main() {
   // Publish to gh-pages branch
   await ghPages.publish(distPath);
   console.log('Published!');
-}
-
-main();
+})();
